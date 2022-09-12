a5d95529161ee0b7963dfe8320c85b75394d7fb5
kusham <kusham1998@gmail.com>
2022-09-12 23:26:34 +0530

    error handling for login

--- lib/Views/login_view.dart
+++ lib/Views/login_view.dart
@@ -68,8 +68,10 @@ class _LoginViewState extends State<LoginView> {
               } on FirebaseAuthException catch (e) {
                 if (e.code == "user-not-found") {
                   devtool.log("User not found");
+                  await showErrorDialog(context, "User not found");
                 } else if (e.code == "wrong-password") {
-                  devtool.log("wrong password");
+                  // devtool.log("wrong password");
+                  await showErrorDialog(context, "Wrong credentials");
                 }
               }
             },
@@ -85,4 +87,25 @@ class _LoginViewState extends State<LoginView> {
       ),
     );
   }
+}
+
+Future<void> showErrorDialog(
+  BuildContext context,
+  String text,
+) {
+  return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("An error occurred"),
+          content: Text(text),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text("OK"))
+          ],
+        );
+      });
 }
