e2a76e230957b3aeae5489952bfc7417cef4f86e
kusham <kusham1998@gmail.com>
2022-09-15 21:12:55 +0530

    some chnage for verfify email page

--- lib/Views/verify_email_view.dart
+++ lib/Views/verify_email_view.dart
@@ -1,5 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'dart:developer' as devtool show log;
+
+import 'package:privertenotes/constants/routes.dart';
 
 class VerifyEmailView extends StatefulWidget {
   const VerifyEmailView({Key? key}) : super(key: key);
@@ -14,15 +18,28 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
     return Scaffold(
       appBar: AppBar(title: const Text("Verify Email")),
       body: Column(children: [
-        const Text("Please verify your email : "),
+        const Text(
+            "We've sent you an email verification. Please open it to verify your email"),
+        const Text(
+            "If you haven't received verification email yet, press the button"),
         TextButton(
             onPressed: () async {
               final user = FirebaseAuth.instance.currentUser;
-              print(user);
+              devtool.log(user.toString());
 
               await user?.sendEmailVerification();
             },
-            child: const Text("send email verification"))
+            child: const Text("send email verification")),
+        TextButton(
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+              // ignore: use_build_context_synchronously
+              Navigator.of(context).pushNamedAndRemoveUntil(
+                registerRoute,
+                (route) => false,
+              );
+            },
+            child: const Text("Restart"))
       ]),
     );
   }
