2de89015147f9fb20e1ea839238bca41ff783780
kusham <kusham1998@gmail.com>
2022-09-19 23:33:02 +0530

    created the auth user file

--- lib/services/auth/auth_exceptions.dart
+++ lib/services/auth/auth_exceptions.dart
@@ -1,25 +1,18 @@
 //login exception
-class UserNotFoundAuthException implements Exception {
+class UserNotFoundAuthException implements Exception {}
 
-}
-
-class WrongPasswordAuthException implements Exception {
-  
-}
+class WrongPasswordAuthException implements Exception {}
 
 // register exception
 
-class WeakPasswordAuthException implements Exception {
-  
-}
-class EmailAlreadyInUseAuthException implements Exception {
-  
-}
+class WeakPasswordAuthException implements Exception {}
 
-class InvalidEmailAuthException implements Exception {
-  
-}
+class EmailAlreadyInUseAuthException implements Exception {}
+
+class InvalidEmailAuthException implements Exception {}
 
 //generic exception
 
 class GenericAuthException implements Exception {}
+
+class UserNotLogInAuthException implements Exception {}
