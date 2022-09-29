4b502baecd6b2dcaa839e6e5c7314ca638e36c3b
kusham <kusham1998@gmail.com>
2022-09-29 22:59:34 +0530

    add service provider to auth service

--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -1,9 +1,12 @@
 import 'package:privertenotes/services/auth/auth_provider.dart';
 import 'package:privertenotes/services/auth/auth_user.dart';
+import 'package:privertenotes/services/auth/firebase_auth_provider.dart';
 
 class AuthService implements AuthProvider {
   final AuthProvider provider;
   const AuthService(this.provider);
+
+  factory AuthService.firebase() => AuthService(FirebaseAuthProvider());
 
   @override
   Future<AuthUser> createUser(
