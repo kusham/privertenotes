6e7b8d3e86f47c9bdfc1d331915877de240d3b99
kusham <kusham1998@gmail.com>
2022-10-12 23:25:54 +0530

    create note service class

--- lib/services/crud/note_service.dart
+++ lib/services/crud/note_service.dart
@@ -1,8 +1,48 @@
+import 'package:flutter/cupertino.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart'
     show getApplicationDocumentsDirectory;
 import 'package:path/path.dart';
 
+class DatabaseAlreadyOpenException implements Exception {}
+
+class UnableToGetDocumentDirectory implements Exception {}
+
+class DatabaseNotOpen implements Exception {}
+
+class NotesService {
+  Database? _db;
+
+  Future<void> close() async {
+    final db = _db;
+    if (db == null) {
+      throw DatabaseNotOpen();
+    } else {
+      await db.close();
+      _db = null;
+    }
+  }
+
+  Future<void> open() async {
+    if (_db != null) {
+      throw DatabaseAlreadyOpenException();
+    }
+    try {
+      final docsPath = await getApplicationDocumentsDirectory();
+      final dbPath = join(docsPath.path, dbName);
+      final db = await openDatabase(dbPath);
+      _db = db;
+
+      await db.execute(createUserTable);
+
+      await db.execute(createNoteTable);
+    } on MissingPlatformDirectoryException {
+      UnableToGetDocumentDirectory();
+    }
+  }
+}
+
+@immutable
 class DatabaseUser {
   final int id;
   final String email;
@@ -56,8 +96,24 @@ class DatabaseNote {
   int get hashCode => id.hashCode;
 }
 
+const dbName = 'testing.db';
+const noteTable = 'note';
+const userTable = 'user';
 const idColumn = 'id';
 const emailColumn = 'email';
 const userIdColumn = 'user_id';
 const textColumn = 'text';
 const isSyncedWithCloudColumn = 'is_synced_with_cloud';
+const createUserTable = '''CREATE TABLE "user" (
+	"id"	INTEGER NOT NULL,
+	"email"	TEXT NOT NULL UNIQUE,
+	PRIMARY KEY("id" AUTOINCREMENT));''';
+
+const createNoteTable = '''CREATE TABLE " note" (
+	"id"	INTEGER NOT NULL,
+	"user_id"	INTEGER NOT NULL,
+	"text"	TEXT,
+	"is_synced_with_cloud"	INTEGER NOT NULL DEFAULT 0,
+	CONSTRAINT "note_PK" PRIMARY KEY("id" AUTOINCREMENT),
+	FOREIGN KEY("user_id") REFERENCES "user"("id")
+);''';
