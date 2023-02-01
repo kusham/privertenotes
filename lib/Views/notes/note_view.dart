94a2e61fcc0d849052926d2e4498c7898569233a
kusham <kusham1998@gmail.com>
2023-02-01 23:57:34 +0530

    add note title

--- lib/Views/notes/note_view.dart
+++ lib/Views/notes/note_view.dart
@@ -74,7 +74,10 @@ class _NotesViewState extends State<NotesView> {
                             return ListView.builder(
                               itemCount: allNotes.length,
                               itemBuilder: (context, index) {
-                                return const Text("Item");
+                                final note = allNotes[index];
+                                return ListTile(
+                                  title: Text(note.text),
+                                );
                               },
                             );
                           } else {
