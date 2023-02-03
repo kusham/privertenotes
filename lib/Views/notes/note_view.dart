70ee0828d4ef3090fd0bb3df253eb8bcae7957b0
kusham <kusham1998@gmail.com>
2023-02-03 20:39:27 +0530

    remove showLogOutDialog

--- lib/Views/notes/note_view.dart
+++ lib/Views/notes/note_view.dart
@@ -101,25 +101,25 @@ class _NotesViewState extends State<NotesView> {
   }
 }
 
-Future<bool> showLogOutDialog(BuildContext context) {
-  return showDialog<bool>(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text("Sign Out"),
-          content: const Text("Are you sure you want to sign out ?"),
-          actions: [
-            TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop(false);
-                },
-                child: const Text('Cancel')),
-            TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop(true);
-                },
-                child: const Text('Log out')),
-          ],
-        );
-      }).then((value) => value ?? false);
-}
+// Future<bool> showLogOutDialog(BuildContext context) {
+//   return showDialog<bool>(
+//       context: context,
+//       builder: (context) {
+//         return AlertDialog(
+//           title: const Text("Sign Out"),
+//           content: const Text("Are you sure you want to sign out ?"),
+//           actions: [
+//             TextButton(
+//                 onPressed: () {
+//                   Navigator.of(context).pop(false);
+//                 },
+//                 child: const Text('Cancel')),
+//             TextButton(
+//                 onPressed: () {
+//                   Navigator.of(context).pop(true);
+//                 },
+//                 child: const Text('Log out')),
+//           ],
+//         );
+//       }).then((value) => value ?? false);
+// }
