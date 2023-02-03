b2964858533fe2335df8ac52904cd963b039e6b5
kusham <kusham1998@gmail.com>
2023-02-03 19:58:37 +0530

    bug fix

--- lib/services/crud/note_service.dart
+++ lib/services/crud/note_service.dart
@@ -295,12 +295,12 @@ const emailColumn = 'email';
 const userIdColumn = 'user_id';
 const textColumn = 'text';
 const isSyncedWithCloudColumn = 'is_synced_with_cloud';
-const createUserTable = '''CREATE TABLE "user" (
+const createUserTable = '''CREATE TABLE IF NOT EXISTS "user" (
 	"id"	INTEGER NOT NULL,
 	"email"	TEXT NOT NULL UNIQUE,
 	PRIMARY KEY("id" AUTOINCREMENT));''';
 
-const createNoteTable = '''CREATE TABLE " note" (
+const createNoteTable = '''CREATE TABLE IF NOT EXISTS "note" (
 	"id"	INTEGER NOT NULL,
 	"user_id"	INTEGER NOT NULL,
 	"text"	TEXT,
