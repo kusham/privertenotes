2297a6eab467ff13f156ab2e611f430ce3ca9559
kusham <kusham1998@gmail.com>
2023-02-12 12:29:59 +0530

    minor change

--- lib/utilities/generic_dialog.dart
+++ lib/utilities/generic_dialog.dart
@@ -1,25 +1,33 @@
-
-
 import 'package:flutter/material.dart';
 
 typedef DialogOptionBuilder<T> = Map<String, T?> Function();
-Future<T?> showGenericDialog<T>(
+
+Future<T?> showGenericDialog<T>({
   required BuildContext context,
   required String title,
   required String content,
   required DialogOptionBuilder optionBuilder,
-) {
- final options = optionBuilder();
- return showDialog(context: context, builder: (content) {
-  return AlertDialog(
-    title: Text(title),
-    content: Text(content),
-    actions: options.keys.map((optionTitle) => {
-      final T value = options[optionTitle];
-      return TextButton(onPressed: () {
-        
-      })
-    }),
-  );
- });
+}) {
+  final options = optionBuilder();
+  return showDialog<T>(
+      context: context,
+      builder: (content) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(content),
+          actions: options.keys.map((optionTitle) {
+            final value = options[optionTitle];
+            return TextButton(
+              onPressed: () {
+                if (value != null) {
+                  Navigator.of(context).pop(value);
+                } else {
+                  
+                }
+              },
+              child: Text(optionTitle),
+            );
+          }),
+        );
+      });
 }
