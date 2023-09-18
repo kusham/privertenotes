5a7e1c0c832c01bb0b17b6c9a3d1c866c3731764
kusham <kusham1998@gmail.com>
2023-09-18 23:26:23 +0530

    minor change

--- lib/Views/login_view.dart
+++ lib/Views/login_view.dart
@@ -4,8 +4,7 @@ import 'dart:developer' as devtool show log;
 import 'package:privertenotes/constants/routes.dart';
 import 'package:privertenotes/services/auth/auth_exceptions.dart';
 import 'package:privertenotes/services/auth/auth_service.dart';
-
-import '../utilities/show_error_dialog.dart';
+import 'package:privertenotes/utilities/dialogs/error_dialog.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
