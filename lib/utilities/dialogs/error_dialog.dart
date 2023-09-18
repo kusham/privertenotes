68c81c258a4f316ff35779731e8ab480390a42df
kusham <kusham1998@gmail.com>
2023-09-18 23:25:37 +0530

    create showErrorDialog

--- lib/utilities/dialogs/error_dialog.dart
+++ lib/utilities/dialogs/error_dialog.dart
@@ -1,10 +1,13 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:privertenotes/utilities/dialogs/generic_dialog.dart';
 
-Future<void> showErrorDialog(
-  BuildContext context,
-  String text
-) {
-  return showGeneralDialog(context: context, pageBuilder: pageBuilder)
+Future<void> showErrorDialog(BuildContext context, String text) {
+  return showGenericDialog(
+      context: context,
+      title: "An error occurred",
+      contents: text,
+      optionBuilder: () => {
+            "OK": null,
+          });
 }
+ 
